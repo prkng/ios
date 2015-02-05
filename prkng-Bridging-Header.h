e29ae2bcf0b145c9383ebc2043a94d31a45e96b7
Cagdas Altinkaya <cagdas@rgbm.co>
2015-02-05 16:39:59 +0200

    add to source control

---
+++ prkng-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  prkng-Bridging-Header.h
+//  prkng-ios
+//
+//  Created by Cagdas Altinkaya on 05/02/15.
+//  Copyright (c) 2015 PRKNG. All rights reserved.
+//
+
+//#ifndef prkng_ios_prkng_Bridging_Header_h
+//#define prkng_ios_prkng_Bridging_Header_h
+//
+//
+//#endif
+#import "Mapbox.h"
--- prkng-ios/View Controllers/Map/MapViewController.swift
+++ prkng-ios/View Controllers/Map/MapViewController.swift
@@ -1,25 +1,31 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  prkng-ios
 //
-//  Created by Cagdas Altinkaya on 26/01/15.
+//  Created by Cagdas Altinkaya on 05/02/15.
 //  Copyright (c) 2015 PRKNG. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: AbstractViewController {
+    
 
+    
+    override func loadView() {
+        self.view = UIView();
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.redColor()
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
