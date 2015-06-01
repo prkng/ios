e040e779d4cd42e9563c39867b7fec9cbd58e40f
Cagdas Altinkaya <cagdas@rgbm.co>
2015-06-01 20:14:12 +0300

    tutorial screens

---
+++ prkng-ios/View Controllers/Here/HereFirstUseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HereFirstUseViewController.swift
+//  prkng-ios
+//
+//  Created by Cagdas Altinkaya on 28/05/15.
+//  Copyright (c) 2015 PRKNG. All rights reserved.
+//
+
+import UIKit
+
+class HereFirstUseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
